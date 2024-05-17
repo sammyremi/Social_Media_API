b1aabba18260a47da1d9c6c2142b46e3f2041681
sammyremi <adebayorsamuelas@gmail.com>
2024-05-17 12:22:42 +0100

    fix user registration

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,25 +4,19 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  private
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def respond_with(resource, options = {})
+    render json: { status: { message: "signed in successfully", data: resource}}, status: :ok
+    puts current_user.inspect
+  end
 
-  # protected
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    puts current_user.inspect
+  end
+
 end
