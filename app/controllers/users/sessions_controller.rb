b8cb6177b49e18885c1a8dab949d8ee30f32e018
sammyremi <adebayorsamuelas@gmail.com>
2024-05-21 16:51:29 +0100

    fix verify jti token

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,10 +5,11 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   skip_before_action :verify_signed_out_user, only: [:destroy]
 
+
   def destroy
+
     if user_signed_in?
-      sign_out(current_user)
-      render json: { message: "user sign_out successfully"}, status: :ok
+      verify_user_token_and_logout
     else
       render json: { message: "no active session"}, status: :unauthorized
     end
@@ -19,7 +20,22 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, options = {})
     render json: { status: { message: "signed in successfully", data: resource}}, status: :ok
-    puts user_signed_in?, current_user
+    
+  end
+
+  def verify_user_token_and_logout
+    if request.headers['Authorization'].present?
+      begin
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+        current_user = User.find(jwt_payload['sub'])
+        sign_out(current_user)
+        render json: { message: "user sign_out successfully"}, status: :ok
+      rescue => e
+        render json: {message: e}, status: :unauthorized
+      end
+    else
+      render json: {message: "missing token"}, status: :unauthorized
+    end
   end
 
 end
