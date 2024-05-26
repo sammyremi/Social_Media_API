2b6d8f89e48e4c9ebbdb1106e189e6d65709c096
sammyremi <adebayorsamuelas@gmail.com>
2024-05-26 17:31:29 +0100

    post model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
