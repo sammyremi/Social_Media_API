8b53a7c8d165ffaec3b1a7051bdb57c06eb51826
sammyremi <adebayorsamuelas@gmail.com>
2024-05-27 05:50:25 +0100

    update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,42 @@
 class PostsController < ApplicationController
+
+    respond_to :json
+
+    def index
+        @posts = Post.all
+    end
+
+    def create
+        @post = Post.create(post_params)
+        if @post.persisted?
+          render json: { message: "post created", post: @post }, status: :created
+        else
+          render json: { message: @post.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @post = set_post_id
+    end
+
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+    private 
+
+
+    def post_params
+        params.require(:post).permit(:title, :id, :user_id)
+    end
+
+    def set_post_id
+        @post = Post.find(post_params[:id])
+    end
+
+    
 end
