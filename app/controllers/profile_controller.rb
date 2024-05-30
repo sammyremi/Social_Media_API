58385009bde1d5e02b9acdc35a28f813e5445523
sammyremi <adebayorsamuelas@gmail.com>
2024-05-30 12:41:50 +0100

    followability migration

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,19 @@
 class ProfileController < ApplicationController
+
+    before_action :authenticate_user!
+
+    before_action :set_user
+
+
+    def follow
+        status = current_user.following?(@user)
+        render json: {message: status}
+    end
+
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
