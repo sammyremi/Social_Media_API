5432e367c73d4d674230af0f52ba6a93c030875e
sammyremi <adebayorsamuelas@gmail.com>
2024-05-30 12:06:16 +0100

    profile controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   }
 
   resources :posts
+
+  post "/profile/:id/follow", to: 'profile#follow', as: 'follow'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,3 +18,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
