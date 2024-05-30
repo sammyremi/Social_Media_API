fdcdf26a6b8df68fa051e6f95485b59dab71af9e
sammyremi <adebayorsamuelas@gmail.com>
2024-05-30 13:30:46 +0100

    accept follow request

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,9 +6,15 @@ class ProfileController < ApplicationController
 
 
     def follow
-        current_user.send_follow_request_to(@user)
+        current_user.send_follow_request_to(@user) if current_user
         render json: {message: " #{current_user.email} sent follow request sent to #{@user.email}"}, status: :ok
     end
+
+    def accept
+        current_user.accept_follow_request_of(@user)
+        render json: {meassage: "You are now following #{@user.email}"}, status: :ok
+    end
+
 
     private
 
