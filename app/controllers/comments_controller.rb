6885eed05df33ea8d039ed01db025ff51180c4eb
sammyremi <adebayorsamuelas@gmail.com>
2024-06-06 07:15:46 +0100

    set comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+
+    before_action :authenticate_user!
+
+    def index
+
+    end
+
+    def create
+        puts comment_params
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:text, :user_id, :post_id)
+    end
 end
