ec6b50e4ad7c94d4e768c85fe7f3d868cc64988f
sammyremi <adebayorsamuelas@gmail.com>
2024-06-06 22:45:22 +0100

    user can create and update comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,36 @@ class CommentsController < ApplicationController
 
     before_action :authenticate_user!
     before_action :find_post
+    before_action :find_comment, only: [:update]
 
     def index
 
     end
 
     def create
-        puts comment_params, params[:post_id], "kkkk"
-
-        render json: @post
+        @post_comment = @post.comments.create(comment_params)
+        if @post_comment.persisted?
+            render json: {response: "created successfully"}, status: :created
+        else
+            render json: {response: "not created", error: @post_comment.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     def show
     end
 
+    def update
+        render json: @comments
+        if @comment.update(comment_params)
+            render json: {response: "updated comment successfully", comment: @comment}, status: :ok
+        else
+            render json: {response: "can't update comment", error: @comment.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+
     def destroy
+
     end
 
 
@@ -28,5 +43,9 @@ class CommentsController < ApplicationController
 
     def find_post
         @post = Post.find(params[:post_id])
+    end
+
+    def find_comment
+        @comment = @post.comments.find(params[:id])
     end
 end
