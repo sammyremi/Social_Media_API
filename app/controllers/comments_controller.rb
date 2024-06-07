df6c44b444dce1bdff9c68d14b4572b80a7fd931
sammyremi <adebayorsamuelas@gmail.com>
2024-06-07 01:13:08 +0100

    user can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,26 +2,26 @@ class CommentsController < ApplicationController
 
     before_action :authenticate_user!
     before_action :find_post
-    before_action :find_comment, only: [:update]
+    before_action :find_comment, only: [:update, :destroy]
 
     def index
-
+        render json: {comments: @post.comments}
     end
 
     def create
         @post_comment = @post.comments.create(comment_params)
         if @post_comment.persisted?
-            render json: {response: "created successfully"}, status: :created
+            render json: {response: "created successfully", comment: @post_comment}, status: :created
         else
             render json: {response: "not created", error: @post_comment.errors.full_messages}, status: :unprocessable_entity
         end
     end
 
     def show
+        render json: find_comment
     end
 
     def update
-        render json: @comments
         if @comment.update(comment_params)
             render json: {response: "updated comment successfully", comment: @comment}, status: :ok
         else
@@ -31,6 +31,9 @@ class CommentsController < ApplicationController
 
 
     def destroy
+        if @comment.destroy
+            render json: {response: "comment has been deleted"}
+        end
 
     end
 
@@ -48,4 +51,5 @@ class CommentsController < ApplicationController
     def find_comment
         @comment = @post.comments.find(params[:id])
     end
+
 end
