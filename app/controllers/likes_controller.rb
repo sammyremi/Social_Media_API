50a034071de2bd3a8edffa2c6d2811d9ead18ef2
sammyremi <adebayorsamuelas@gmail.com>
2024-06-13 21:48:19 +0100

    user cannot like likeable twice

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,39 @@
 class LikesController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_likeable
+    before_action :set_likeable, only: [:create]
+    before_action :find_like, only: [:destroy]
 
+    #create like for post or comment if it does not exist
     def create
-        @like = Like.new(user: current_user, likeable: @like_item)
-        if @like.save
-            render json: {response: "Like created successfully"}, status: :ok
+        if @like_item.likes.exists?(user_id: current_user.id)
+            render json: {message: "you already liked this"}, status: :unprocessable_entity
+    
         else
-            render json: {response: @like.errors.full_messages}, status: :unprocessable_entity
+            @like = Like.new(user: current_user, likeable: @like_item)
+            if @like.save
+                render json: {message: "you liked #{@like}"}, status: :ok
+            else
+                render json: {response: @like.errors.full_messages}, status: :unprocessable_entity
+            end
         end
     end
 
     def destroy
+
+        puts params
     end
 
     private
 
-    # def likes_params
-
-    # end
-
     def set_likeable
+        #find the post to like
         @post = Post.find(params[:post_id])
 
+        #get comment and post id
         @post_id = params[:post_id]
         @comment_id = params[:comment_id]
 
+        # set like to either post or comment
         if @comment_id.present?
             @like_item = @post.comments.find(@comment_id)
         else
@@ -36,6 +44,12 @@ class LikesController < ApplicationController
     def find_like
         
     end
+
+    # def already_liked
+
+    #     @like_item.likes.exists?(user_id: current_user.id)
+
+    # end
 
     
 end
